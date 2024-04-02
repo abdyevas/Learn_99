a048e13d0ba43343208e816f443cf4201a1c1335
abdyevas <sabina.abdyeva@ufaz.az>
2024-04-02 23:43:13 +0400

    add fetch json

--- function.js
+++ function.js
@@ -1,3 +1,26 @@
+fetch('names.json')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log('JSON data:', data);
+        
+        var namesEn = data.map(obj => obj.en);
+        var namesAr = data.map(obj => obj.ar);
+
+        var randomInx = Math.floor(Math.random() * namesEn.length);
+        var randomNameEn = namesEn[randomInx];
+        var randomNameAr = namesAr[randomInx];
+
+        var frontText = document.getElementById('front-text');
+        frontText.textContent = randomNameEn;
+        frontText.setAttribute('data-translation', randomNameAr);
+    })
+    .catch(error => console.error('Error loading JSON:', error));
+
 function flipCard() {
     var card = document.getElementById('card');
     var rotation = getComputedStyle(card).getPropertyValue('transform');
@@ -11,10 +34,10 @@ function flipCard() {
 
     setTimeout(function() {
         if (rotation === 'none' || rotation === 'matrix(1, 0, 0, 1, 0, 0)') {
-            frontText.textContent = 'TRANSLATION';
+            frontText.textContent = frontText.getAttribute('data-translation');
             frontText.style.transform = 'scaleY(-1)';
         } else {
-            frontText.textContent = 'Word';
+            frontText.textContent = frontText.getAttribute('data-name');
             frontText.style.transform = 'none';
         }
     }, 100);
